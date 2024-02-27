b41b0515ddc929f01b5246cac5f8131298a7c31c
李元琦 <yuan.chi@program.com.tw>
2024-02-27 17:22:06 +0800

    feat: 如果已被取代，則不列入當月統計

--- src/routes/postback/monthTotal.js
+++ src/routes/postback/monthTotal.js
@@ -4,15 +4,22 @@ const { getenv, getFormDataNoCache } = require('../../utils/helpers')
 const flexMonthTotal = require('../../flexMessage/monthTotal')
 
 module.exports = async ({ event, req, args }) => {
-  const datas = _.map(await getFormDataNoCache(getenv('ACCOUNTING_CSV')), data => ({
-    ...data,
-    money: _.parseInt(data.money),
-  }))
   const [yearMonth] = args
+  const coveredIds = []
 
-  const monthTotalData = _.filter(datas, item => {
-    return _.includes(item.date, yearMonth)
-  })
+  const datas = await getFormDataNoCache(getenv('ACCOUNTING_CSV'))
+  const monthTotalData = _.chain(datas)
+    .filter(item => { return _.includes(item.date, yearMonth) })
+    .orderBy('id', 'desc')
+    .map(item => {
+      if (item?.coverId) coveredIds.push(item?.coverId)
+      if (_.includes(coveredIds, item?.coverId)) return null
+      return {
+        ...item,
+        money: _.parseInt(item.money),
+      }
+    })
+    .value()
 
   return await client.validateAndReplyMessage(event.replyToken, flexMonthTotal({ monthTotalData, yearMonth }))
 }
