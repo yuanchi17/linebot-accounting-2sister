a578044be331f2fbf9afb8b74c35da4896d383f5
李元琦 <yuan.chi@program.com.tw>
2024-11-18 13:41:46 +0800

    feat: 調整寫法

--- src/routes/postback/coverOldDatas.js
+++ src/routes/postback/coverOldDatas.js
@@ -1,6 +1,9 @@
 const _ = require('lodash')
+const { client } = require('../../utils/lineat')
 const { getFormDataNoCache, getenv } = require('../../utils/helpers')
 const AccountingService = require('../../services/accounting')
+const flexAddItems = require('../../flexMessage/addItems')
+const flexText = require('../../flexMessage/text')
 
 module.exports = async ({ event, req, args }) => {
   const [checkCoverInfo] = args
@@ -8,5 +11,16 @@ module.exports = async ({ event, req, args }) => {
   const newDatas = _.orderBy(_.filter(await getFormDataNoCache(getenv('ACCOUNTING_TEMP_CSV')), data => _.includes(data.id, checkCoverInfo.id), 'id', 'asc'))
 
   const isExist = _.find(oldDatas, oldData => _.includes(oldData.id, checkCoverInfo.id))
-  await AccountingService.sendGoogleFormByTempData({ event, newDatas, isExist })
+  if (isExist) return await client.validateAndReplyMessage(event.replyToken, flexText('此筆已紀錄過'))
+
+  await AccountingService.sendTextGoogleForm({
+    text: newDatas[0]?.text,
+    textId: newDatas[0]?.textId,
+  })
+  for (const data of newDatas) {
+    data.money = _.parseInt(data.money)
+    await AccountingService.sendGoogleFormByItem(data)
+  }
+
+  return await client.validateAndReplyMessage(event.replyToken, flexAddItems(_.groupBy(newDatas, 'type')))
 }
--- src/services/accounting.js
+++ src/services/accounting.js
@@ -48,67 +48,73 @@ exports.parseText = ({ textArr, textId }) => {
 
     obj[type].push(item)
   })
-  return obj
+  return { obj, accountDate }
 }
 
-exports.checkSameDatas = async itemsObj => {
-  // 取得當日的資料，並由新到舊排序
-  const oldDatas = _.orderBy(_.filter(await exports.getAccountDatas(), ['date', getNowDate()]), 'id', 'desc')
-  // 若當日無舊資料，則不需核對是否有重複
+exports.checkSameDatas = async (itemsObj, accountDate) => {
+  // 取得當筆記帳日期的資料，並由新到舊排序
+  const oldDatas = _.orderBy(_.filter(await exports.getAccountDatas(), ['date', accountDate]), 'id', 'desc')
+  // 若當筆記帳日期無舊資料，則不需核對是否有重複
   if (!oldDatas.length) return
 
-  const sameTitleAndMoneyIds = []
+  const sameDataIds = []
   let hasSameData = false
   for (const [type, items] of _.toPairs(itemsObj)) {
     for (let i = 0; i < items.length; i++) {
+      const newData = items[i]
       // 如果有相同標題，跟使用者確認是否需要覆蓋資料
-      const sameData = _.find(oldDatas, ['title', items[i].title])
+      const sameData = _.find(oldDatas, ['title', newData.title])
       if (!sameData) continue
-      if (items[i].money === sameData.money && items[i].ps === sameData.ps) {
-        sameTitleAndMoneyIds.push(items[i].id)
+
+      if (newData.money === sameData.money && newData.ps === sameData.ps) {
+        sameDataIds.push(newData.id)
         continue
       }
       hasSameData = true
       itemsObj[type][i] = {
-        ...items[i],
+        ...newData,
         sameData,
       }
     }
   }
 
-  // 若標題及金額皆相同，則移除
-  if (sameTitleAndMoneyIds.length) {
-    _.remove(itemsObj['支出'], i => _.includes(sameTitleAndMoneyIds, i.id))
-    _.remove(itemsObj['收入'], i => _.includes(sameTitleAndMoneyIds, i.id))
+  // 只保留需詢問是否要覆蓋的資料
+  if (sameDataIds.length) {
+    _.remove(itemsObj['支出'], i => _.includes(sameDataIds, i.id))
+    _.remove(itemsObj['收入'], i => _.includes(sameDataIds, i.id))
   }
   return hasSameData
 }
 
-exports.sendGoogleForm = async ({ event, itemsObj }) => {
+exports.sendGoogleForm = async ({ itemsObj }) => {
   for (const [type, items] of _.toPairs(itemsObj)) {
     for (let i = 0; i < items.length; i++) {
-      try {
-        const params = {}
-        params[`entry.${getenv('ACCOUNTING_ITEM_ID')}`] = items[i].id
-        params[`entry.${getenv('ACCOUNTING_ITEM_DATE')}`] = items[i].date
-        params[`entry.${getenv('ACCOUNTING_ITEM_MONEY')}`] = items[i].money
-        params[`entry.${getenv('ACCOUNTING_ITEM_PS')}`] = items[i].ps
-        params[`entry.${getenv('ACCOUNTING_ITEM_TITLE')}`] = items[i].title
-        params[`entry.${getenv('ACCOUNTING_ITEM_COVER_ID')}`] = items[i]?.sameData?.id || ''
-        params[`entry.${getenv('ACCOUNTING_ITEM_TYPE')}`] = type
-        params[`entry.${getenv('ACCOUNTING_ITEM_TEXT_ID')}`] = items[i].textId
-
-        await axios.post(`https://docs.google.com/forms/d/e/${getenv('GOOGLE_FORM_ID')}/formResponse`, httpBuildQuery(params))
-      } catch (err) {
-        log(errToPlainObj(err))
-      }
+      const item = { ...items[i], type, coverId: items[i]?.sameData?.id || '' }
+      await exports.sendGoogleFormByItem(item)
       itemsObj[type][i] = items[i]
     }
   }
-  return await client.validateAndReplyMessage(event.replyToken, flexAddItems(itemsObj))
 }
 
-exports.sendTempGoogleForm = async ({ itemsObj }) => {
+exports.sendGoogleFormByItem = async item => {
+  try {
+    const params = {}
+    params[`entry.${getenv('ACCOUNTING_ITEM_ID')}`] = item.id
+    params[`entry.${getenv('ACCOUNTING_ITEM_DATE')}`] = item.date
+    params[`entry.${getenv('ACCOUNTING_ITEM_MONEY')}`] = item.money
+    params[`entry.${getenv('ACCOUNTING_ITEM_PS')}`] = item.ps
+    params[`entry.${getenv('ACCOUNTING_ITEM_TITLE')}`] = item.title
+    params[`entry.${getenv('ACCOUNTING_ITEM_COVER_ID')}`] = item?.coverId
+    params[`entry.${getenv('ACCOUNTING_ITEM_TYPE')}`] = item.type
+    params[`entry.${getenv('ACCOUNTING_ITEM_TEXT_ID')}`] = item.textId
+
+    await axios.post(`https://docs.google.com/forms/d/e/${getenv('GOOGLE_FORM_ID')}/formResponse`, httpBuildQuery(params))
+  } catch (err) {
+    log(errToPlainObj(err))
+  }
+}
+
+exports.sendTempGoogleForm = async ({ itemsObj, text }) => {
   for (const [type, items] of _.toPairs(itemsObj)) {
     for (let i = 0; i < items.length; i++) {
       try {
@@ -120,6 +126,7 @@ exports.sendTempGoogleForm = async ({ itemsObj }) => {
         params[`entry.${getenv('ACCOUNTING_ITEM_TITLE')}`] = items[i].title
         params[`entry.${getenv('ACCOUNTING_ITEM_COVER_ID')}`] = items[i]?.sameData?.id || ''
         params[`entry.${getenv('ACCOUNTING_ITEM_TYPE')}`] = type
+        params[`entry.${getenv('ACCOUNTING_ITEM_TEXT')}`] = text
         params[`entry.${getenv('ACCOUNTING_TEMP_ITEM_TEXT_ID')}`] = items[i]?.textId
 
         await axios.post(`https://docs.google.com/forms/d/e/${getenv('GOOGLE_FORM_TEMP_ID')}/formResponse`, httpBuildQuery(params))
@@ -128,29 +135,6 @@ exports.sendTempGoogleForm = async ({ itemsObj }) => {
       }
     }
   }
-}
-
-exports.sendGoogleFormByTempData = async ({ event, newDatas, isExist }) => {
-  if (isExist) return await client.validateAndReplyMessage(event.replyToken, flexText('此筆已紀錄過'))
-  for (const data of newDatas) {
-    data.money = _.parseInt(data.money)
-    try {
-      const params = {}
-      params[`entry.${getenv('ACCOUNTING_ITEM_ID')}`] = data.id
-      params[`entry.${getenv('ACCOUNTING_ITEM_DATE')}`] = data.date
-      params[`entry.${getenv('ACCOUNTING_ITEM_MONEY')}`] = data.money
-      params[`entry.${getenv('ACCOUNTING_ITEM_PS')}`] = data.ps
-      params[`entry.${getenv('ACCOUNTING_ITEM_TITLE')}`] = data.title
-      params[`entry.${getenv('ACCOUNTING_ITEM_COVER_ID')}`] = data?.coverId
-      params[`entry.${getenv('ACCOUNTING_ITEM_TYPE')}`] = data.type
-      params[`entry.${getenv('ACCOUNTING_ITEM_TEXT')}`] = data.text
-
-      await axios.post(`https://docs.google.com/forms/d/e/${getenv('GOOGLE_FORM_ID')}/formResponse`, httpBuildQuery(params))
-    } catch (err) {
-      log(errToPlainObj(err))
-    }
-  }
-  return await client.validateAndReplyMessage(event.replyToken, flexAddItems(_.groupBy(newDatas, 'type')))
 }
 
 exports.sendTextGoogleForm = async ({ textId, text }) => {
@@ -169,19 +153,21 @@ exports.main = async ({ event, req, text }) => {
   const textId = dayjs().unix()
   const textArr = _.compact(text.split('\n'))
 
-  const itemsObj = exports.parseText({ textArr, textId })
+  const { obj: itemsObj, accountDate } = exports.parseText({ textArr, textId })
   if (!itemsObj) return
 
-  const hasSameData = await exports.checkSameDatas(itemsObj)
+  const hasSameData = await exports.checkSameDatas(itemsObj, accountDate)
   if (hasSameData) {
-    await exports.sendTempGoogleForm({ itemsObj })
+    await exports.sendTempGoogleForm({ itemsObj, text })
     return await client.validateAndReplyMessage(event.replyToken, flexCheckSameItems({ itemsObj }))
   }
 
   if (!itemsObj['收入']?.length && !itemsObj['支出']?.length) return await client.validateAndReplyMessage(event.replyToken, flexText('這些項目可能已經記錄過，或是格式輸入錯誤(被你氣鼠XD)'))
 
   await Promise.all([
-    exports.sendTextGoogleForm({ event, textId, text }),
-    exports.sendGoogleForm({ event, itemsObj }),
+    exports.sendTextGoogleForm({ textId, text }),
+    exports.sendGoogleForm({ itemsObj }),
   ])
+
+  return await client.validateAndReplyMessage(event.replyToken, flexAddItems(itemsObj))
 }
